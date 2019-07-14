d8d1f6b52f66e97fe7633349f164f1042ba5be7b
JuanIrache <yrache@gmail.com>
2019-07-14 04:55:07 +0200

    visual changes

--- bar_chart/bars.js
+++ bar_chart/bars.js
@@ -1,4 +1,4 @@
-const { select, scaleLinear, csv, max, scaleBand, axisLeft, axisBottom } = d3;
+const { select, scaleLinear, csv, max, scaleBand, axisLeft, axisBottom, format } = d3;
 const svg = select('svg');
 const width = +svg.attr('width');
 const height = +svg.attr('height');
@@ -8,7 +8,7 @@ const render = data => {
   const xVal = d => d.population;
   const yVal = d => d.country;
 
-  const margin = { top: 20, bottom: 20, left: 90, right: 50 };
+  const margin = { top: 60, bottom: 50, left: 150, right: 50 };
 
   const g = svg.append('g').attr(
     'transform',
@@ -16,6 +16,12 @@ const render = data => {
       scale(${(width - margin.left - margin.right) / width},\
       ${(height - margin.top - margin.bottom) / height})`
   );
+
+  g.append('text')
+    .text('Bar chart')
+    .attr('class', 'top-title')
+    .attr('x', width / 2)
+    .attr('y', -15);
 
   const xScale = scaleLinear()
     .domain([0, max(data, xVal)])
@@ -26,10 +32,25 @@ const render = data => {
     .range([0, height])
     .padding(0.05);
 
-  g.append('g').call(axisLeft(yScale));
   g.append('g')
+    .call(axisLeft(yScale))
+    .selectAll('.domain, .tick line')
+    .remove();
+
+  const xAxis = axisBottom(xScale).tickFormat(input => format('.3s')(input).replace(/G$/, 'B'));
+  const bottomG = g
+    .append('g')
     .attr('transform', `translate(0,${height})`)
-    .call(axisBottom(xScale));
+    .call(xAxis);
+  bottomG.select('.domain').remove();
+  bottomG.selectAll('.tick line').attr('y2', -height);
+
+  bottomG
+    .append('text')
+    .text('Population')
+    .attr('class', 'label')
+    .attr('x', width / 2)
+    .attr('y', +60);
 
   g.selectAll('rect')
     .data(data)
