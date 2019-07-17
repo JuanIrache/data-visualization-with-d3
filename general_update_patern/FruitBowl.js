e9c3ceba8476027098ede93d7ca5e82c2fff78c6
JuanIrache <yrache@gmail.com>
2019-07-17 07:33:44 +0200

    one direction data flow, mouse events, single datum join

--- general_update_patern/FruitBowl.js
+++ general_update_patern/FruitBowl.js
@@ -7,14 +7,70 @@ const radius = scaleOrdinal()
   .domain(['lemon', 'apple'])
   .range([15, 30]);
 
-export default (selection, { fruits, height }) => {
-  const groups = selection.selectAll('g').data(fruits);
+const rectSize = { w: 395, h: 80 };
+
+export default (selection, { height, width, fruits, selected, changeType, removeOne, selectOne }) => {
+  selection
+    .selectAll('rect')
+    .data([null])
+    .enter()
+    .append('rect')
+    .attr('width', rectSize.w)
+    .attr('height', rectSize.h)
+    .attr('y', height / 2 - rectSize.h / 2)
+    .attr('x', width / 2 - rectSize.w / 2)
+    .attr('rx', rectSize.h / 2);
+
+  const translateX = (d, i) => `translate(${width / 2 - rectSize.w / 2 + 60 + i * 70},${height / 2})`;
+
+  const groups = selection.selectAll('g').data(fruits, d => d.id);
+
   const groupsEnter = groups.enter().append('g');
-  groupsEnter.merge(groups).attr('transform', (d, i) => `translate(${60 + i * 90},${height / 2})`);
-  groups.exit().remove();
+
   groupsEnter
-    .append('circle')
-    .merge(groups.select('circle'))
+    .attr('transform', translateX)
+    .merge(groups)
+    .transition()
+    .duration(600)
+    .attr('transform', translateX);
+
+  const groupsExit = groups.exit();
+
+  groupsExit
+    .transition()
+    .duration(600)
+    .remove();
+
+  const activeCircles = groupsEnter.append('circle').merge(groups.select('circle'));
+
+  activeCircles
+    .attr('stroke', d => (d.id === selected ? 'white' : 'none'))
+    .transition()
+    .duration(600)
     .attr('r', d => radius(d.type))
     .attr('fill', d => color(d.type));
+
+  activeCircles
+    .on('mouseover', d => selectOne(d.id))
+    .on('mouseout', d => selectOne(null))
+    .on('click', d => removeOne(d.id));
+
+  groupsExit
+    .select('circle')
+    .transition()
+    .duration(600)
+    .attr('r', 0);
+
+  groupsEnter
+    .append('text')
+    .merge(groups.select('text'))
+    .text(d => d.type)
+    .attr('y', 55)
+    .on('click', d => changeType(d.id));
+
+  groupsExit
+    .select('text')
+    .transition()
+    .duration(600)
+    .style('opacity', 0);
 };
--- general_update_patern/app.js
+++ general_update_patern/app.js
@@ -5,20 +5,25 @@ const height = +svg.attr('height');
 import FruitBowl from './FruitBowl.js';
 
 let fruits = Array.from('x'.repeat(5)).map(e => ({ type: 'apple', id: Math.random() }));
-fruits[2].type = 'lemon';
+let selected = null;
+
+const changeType = id => {
+  fruits = fruits.map(d => (d.id === id ? { ...d, type: d.type === 'lemon' ? 'apple' : 'lemon' } : d));
+  render();
+};
+
+const removeOne = id => {
+  fruits = fruits.filter(d => d.id !== id);
+  render();
+};
+
+const selectOne = id => {
+  selected = id;
+  render();
+};
 
 const render = () => {
-  FruitBowl(svg, { fruits, height });
+  FruitBowl(svg, { height, width, fruits, selected, changeType, removeOne, selectOne });
 };
 
 render();
-
-window.setTimeout(() => {
-  fruits[3].type = 'lemon';
-  render();
-}, 3000);
-
-window.setTimeout(() => {
-  fruits = fruits.filter((d, i) => i != 1);
-  render();
-}, 6000);
